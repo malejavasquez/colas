4a940d71b2387801a8d41843587ed7632e4cd70c
Maria Alejandra vasquez <159583068+malejavasquez@users.noreply.github.com>
2024-02-14 19:54:56 -0500

    Initial commit

---
+++ src/main/java/cola/Cola.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cola;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author maria_a.vasquez
+ */
+public class Cola <T extends Base> {
+    
+    private LinkedList<T> elementos;
+    
+    public Cola(){
+        elementos=new LinkedList<>();
+    }
+    
+    public void encolar (T elemento){
+        elementos.addLast(elemento);
+    }
+    public T desencolar(){
+        return elementos.removeFirst();
+    }
+    public boolean estaVacia(){
+        return elementos.isEmpty();
+        
+    }
+}
